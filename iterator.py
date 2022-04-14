9dcae5f177a0133406f1637a0add5eac58c699a9
VladimirDanilov97 <dan1997ford@yandex.ru>
2022-04-14 20:02:48 +0300

    del lines for debug

--- iterator.py
+++ iterator.py
@@ -1,39 +1,36 @@
-import time
-
-
 class MyIterator():
     def __init__(self, nested_list):
-        self.current_list = [nested_list]
+        self.iterated_list = [nested_list]
         self.indexes = [0]
-        self.iterators = [self.current_list[0][0].__iter__()]
+        self.iterators = [self.iterated_list[0][0].__iter__()]
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        time.sleep(0.2)
+       
         try:
             n = next(self.iterators[-1])
             if isinstance(n, list):
-                self.current_list.append([n])
+                self.iterated_list.append([n])
                 self.indexes.append(0)
                 self.iterators.append(n.__iter__())
                 return next(self)
             else:
                 return n
-        except StopIteration:
 
-            if len(self.current_list) > 1 and (
-              self.indexes[-1] == len(self.current_list[-1])-1):
-                self.current_list.pop()
+        except StopIteration:
+            if len(self.iterated_list) > 1 and (
+              self.indexes[-1] == len(self.iterated_list[-1])-1):
+                self.iterated_list.pop()
                 self.indexes.pop()
                 self.iterators.pop()
                 return next(self)
 
             self.indexes[-1] += 1
-            if self.indexes[0] == len(self.current_list[0]):
+            if self.indexes[0] == len(self.iterated_list[0]):
                 raise StopIteration
-            self.iterators[-1] = self.current_list[-1][self.indexes[-1]].__iter__()
+            self.iterators[-1] = self.iterated_list[-1][self.indexes[-1]].__iter__()
             return next(self)
 
 
@@ -55,7 +52,7 @@ if __name__ == '__main__':
         ['a', 'b', 'c'],
         ['d', 'e', 'f', 'h', False],
         [1, 2, None],
-        [[5, 6], [[7, 8, [9]], 2]]
+        [[5, 6], [[7, 8, [9]], 2]], 
     ]
 
     iterator = MyIterator(nested_list)

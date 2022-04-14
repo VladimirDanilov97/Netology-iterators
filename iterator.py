c0df67ca7a4e05d78a9177ab615c8eb5c850453b
VladimirDanilov97 <dan1997ford@yandex.ru>
2022-04-14 14:00:19 +0300

    Fix by flake8

--- iterator.py
+++ iterator.py
@@ -1,13 +1,6 @@
 import time
 
 
-nested_list = [
-	['a', 'b', 'c'],
-	['d', 'e', 'f', 'h', False],
-	[1, 2, None],
-    [[5, 6], [[7,8,[9]],2]]
-]
-
 class MyIterator():
     def __init__(self, nested_list):
         self.current_list = [nested_list]
@@ -16,7 +9,6 @@ class MyIterator():
 
     def __iter__(self):
         return self
-    
 
     def __next__(self):
         time.sleep(0.2)
@@ -30,8 +22,9 @@ class MyIterator():
             else:
                 return n
         except StopIteration:
-            
-            if len(self.current_list) > 1 and self.indexes[-1] == len(self.current_list[-1])-1 :
+
+            if len(self.current_list) > 1 and (
+              self.indexes[-1] == len(self.current_list[-1])-1):
                 self.current_list.pop()
                 self.indexes.pop()
                 self.iterators.pop()
@@ -39,10 +32,9 @@ class MyIterator():
 
             self.indexes[-1] += 1
             if self.indexes[0] == len(self.current_list[0]):
-                raise StopIteration 
+                raise StopIteration
             self.iterators[-1] = self.current_list[-1][self.indexes[-1]].__iter__()
             return next(self)
-
 
 
 def generator(nested_list):
@@ -56,8 +48,18 @@ def generator(nested_list):
                     yield item
         else:
             yield list_
-iterator = MyIterator(nested_list)
-g = generator(nested_list)
 
-for i,j in zip(iterator, g):
-    print(f'{i}; {j};')
+
+if __name__ == '__main__':
+    nested_list = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None],
+        [[5, 6], [[7, 8, [9]], 2]]
+    ]
+
+    iterator = MyIterator(nested_list)
+    g = generator(nested_list)
+
+    for i, j in zip(iterator, g):
+        print(f'{i}; {j};')
